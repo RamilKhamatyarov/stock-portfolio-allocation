58e9cdb007cbc8340c2a5ff49e791772848dca8e
ramil-khamatyarov <ramil.hamatyarov@yandex.ru>
2025-10-29 15:41:04 +0300

    Migrate to spring boot 3.5.7 - fix rest client annotations

--- src/main/java/ru/rkhamatyarov/stockportfolioallocation/client/IEXCloudClientImpl.java
+++ src/main/java/ru/rkhamatyarov/stockportfolioallocation/client/IEXCloudClientImpl.java
@@ -1,46 +1,32 @@
 package ru.rkhamatyarov.stockportfolioallocation.client;
 
-import com.sun.jersey.api.client.WebResource;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 import ru.rkhamatyarov.stockportfolioallocation.client.dto.StockCompanySectorDto;
-import ru.rkhamatyarov.stockportfolioallocation.exception.BusinessException;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriBuilder;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-
 
 @Slf4j
 @Component
 public class IEXCloudClientImpl implements IEXCloudClient {
     private final String TOKEN_TEMPLATE = "?token=";
-
-
-    private final Client client;
+    private final RestClient restClient;
     private final String iexCloudHost;
     private final String sectorServicePath;
     private final String latestPriceServicePath;
     private final String securityToken;
 
     public IEXCloudClientImpl(
+            RestClient restClient,
             @Value("${iex.cloud.host}") String iexCloudHost,
             @Value("${iex.cloud.sector.service}") String sectorServicePath,
             @Value("${iex.cloud.price.service}") String latestPriceServicePath,
             @Value("${iex.cloud.token}") String securityToken
     ) {
-        this.client = ClientBuilder.newClient();
+        this.restClient = restClient;
         this.iexCloudHost = iexCloudHost;
-        this.sectorServicePath =sectorServicePath;
+        this.sectorServicePath = sectorServicePath;
         this.latestPriceServicePath = latestPriceServicePath;
 
         checkToken(securityToken);
@@ -49,33 +35,28 @@ public class IEXCloudClientImpl implements IEXCloudClient {
 
     @Override
     public StockCompanySectorDto getSector(@NonNull String companyName) {
-        String restUri = iexCloudHost + sectorServicePath;
+        String restUri = buildUri(iexCloudHost + sectorServicePath, companyName);
 
-        return client
-                .target(mapVariablesToUri(restUri, companyName) + TOKEN_TEMPLATE + securityToken)
-                .path(companyName)
-                .request(MediaType.APPLICATION_JSON)
-                .get(StockCompanySectorDto.class);
-
+        return restClient
+                .get()
+                .uri(restUri + TOKEN_TEMPLATE + securityToken)
+                .retrieve()
+                .body(StockCompanySectorDto.class);
     }
 
     @Override
     public Double getLatestPrice(@NonNull String companyName) {
-        String restUri = iexCloudHost + latestPriceServicePath;
+        String restUri = buildUri(iexCloudHost + latestPriceServicePath, companyName);
 
-        return client
-                .target(mapVariablesToUri(restUri, companyName) + TOKEN_TEMPLATE + securityToken)
-                .path(companyName)
-                .request(MediaType.APPLICATION_JSON)
-                .get(Double.class);
+        return restClient
+                .get()
+                .uri(restUri + TOKEN_TEMPLATE + securityToken)
+                .retrieve()
+                .body(Double.class);
     }
 
-
-    private URI mapVariablesToUri(String restUri, String companyName) {
-        Map<String, String> parameters = new HashMap<>();
-        parameters.put("companyName", companyName);
-        UriBuilder builder = UriBuilder.fromPath(restUri);
-        return builder.buildFromMap(parameters);
+    private String buildUri(String baseUri, String companyName) {
+        return baseUri.replace("{companyName}", companyName);
     }
 
     private void checkToken(String securityToken) throws IllegalStateException {
